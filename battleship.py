67c2e26e92fd34924f022dd10b40f69751cda7d5
Tom Peck <tapeck@umass.edu>
2016-09-20 16:59:01 -0400

    adding board and now moving on to setup

--- battleship.py
+++ battleship.py
@@ -1,2 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python3
+import pdb
 
+def setup():
+    print('player one selct your ships!')
+    for i in range(4):
+        name = input('')
+    
+
+board = Board(16)
+setup()
---
+++ ship.py
@@ -0,0 +1,7 @@
+class ship:
+    def __init__(self, coordinates):
+        self.coordinates = coordinates
+
+    def coordinates():
+        return self.coordinates
+    
