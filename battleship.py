4d35cdb41e5f50c566700f4422c9e6ca0eb90e1d
Tom Peck <tapeck@umass.edu>
2016-09-20 17:04:52 -0400

    basics stubed out

--- battleship.py
+++ battleship.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
 import pdb
 
-def setup():
-    print('player one selct your ships!')
-    for i in range(4):
-        name = input('')
-    
+def setup(ships_each):
+    for j in range(2):
+        print('player' + j + 'selct your ships!')
+        for i in range(ships_each):
+            name = input('Name your ship (must be unique): ')
+            name += j # this is how we identify the ship
+            point = input('Initial point: ')
+            direction = input('Direction (N,S,E,W): ')
+            board.insert_ship(point, direction, 4, name) 
 
 board = Board(16)
 setup()
